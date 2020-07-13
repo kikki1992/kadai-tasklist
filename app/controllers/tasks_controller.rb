d4e685496b319e4600fd8ab5c130de4a8903dc97
yuma.matsuyama <rinmoku4818@yahoo.co.jp>
2020-07-13 05:00:22 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,23 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit ,:update, :destroy ]
   
   def index
-    @tasks = Task.all
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc)
+    end
   end 
   
   def show
   end
   
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = "タスクが正常に追加されました"
@@ -50,7 +54,7 @@ class TasksController < ApplicationController
   private
   #strong parameter
   def task_params
-    params.require(:task).permit(:content, :status)
+    params.require(:task).permit(:content, :status, :users)
   end
   
   def set_task
